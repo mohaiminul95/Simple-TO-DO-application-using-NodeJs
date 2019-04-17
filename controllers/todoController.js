63d7882e7a75e10d292def59322d43540e1e8bc0
mohaiminul <rabidislam94@gmail.com>
2019-04-17 20:17:19 +0600

    Data is inserting and data can be view from Json file

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,22 @@
+var data= [{item:'code'},{item:'eat'},{item:'sleep'},{item:'repeat'}];
+
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
 module.exports= function(app) {
 
 
 app.get('/todo', function(req, res) {
-    res.render('index');
+    res.render('index',{todos:data});
 });
 
-app.post('/todo', function(req, res) {
-
+app.post('/todo', urlencodedParser, function(req, res) {
+    data.push(req.body);
+    res.render('index',{todos:data});
 });
 
-app.delete('/todo', function(req, res) {
+app.delete('/todo/:item', function(req, res) {
 
 });
 
