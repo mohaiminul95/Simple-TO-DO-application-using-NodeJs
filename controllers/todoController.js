44dbe5046b03c069d017552b1f6b55c98b3e5290
mohaiminul <rabidislam94@gmail.com>
2019-04-18 17:23:10 +0600

    single item can be delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,11 +14,13 @@ app.get('/todo', function(req, res) {
 app.post('/todo', urlencodedParser, function(req, res) {
     data.push(req.body);
     res.json(data);
-    // res.render('index',{todos:data});
 });
 
 app.delete('/todo/:item', function(req, res) {
-
+    data= data.filter(function(todo) {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
 });
 
 
